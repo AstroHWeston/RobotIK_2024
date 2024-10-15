9fa39c41f22f702d729049983a85204e37ff0fec
Astro <maxsimilijano@gmail.com>
2024-10-15 15:50:11 +0200

    Add files via upload (#3)
    Kod za ruku od robota
    
    Co-authored-by: Noa-Hr <hrvatnoa@gmail.com>

---
+++ primjeri/ruka_sketch_oct14a.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(19, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(19)==LOW) Serial.println("DA");
+  else Serial.println("NO");
+  delay(500);
+}
